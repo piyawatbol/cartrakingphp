f7cf1db4563b1d8b1bf88a3c7129bd1ac05203bb
Piyawat Sakdadet <piyawatbol@gmail.com>
2022-04-09 11:43:43 +0700

    เพิ่ม where

--- insert_location/insert_location.php
+++ insert_location/insert_location.php
@@ -2,8 +2,9 @@
     require '../connect.php';
     $lati = $_POST['lati'];
     $longti = $_POST['longti'];
+    $user_id = $_POST['user_id'];
 
-    $sql = "UPDATE tb_user SET lati='$lati', longti='$longti'" ;
+    $sql = "UPDATE tb_user SET lati='$lati', longti='$longti' WHERE user_id = $user_id " ;
     $result = mysqli_query($condb,$sql);
 
     if($result){
